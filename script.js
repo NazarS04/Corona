0f17527d0342058ed96fafd6e03f4a9ec81a5587
Nazar <Nazar@MacBook-Pro-Nazar.local>
2023-01-13 19:34:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let header = document.querySelector(".header");
+window.addEventListener("scroll",addRemoveHeaderBackgroundColor);
+
+function addRemoveHeaderBackgroundColor(){
+    if(document.documentElement.clientWidth>767)
+    {
+        if(scrollY>=60 && !header.classList.contains("header-bg-white"))
+        {
+            header.classList.add("header-bg-white")
+        }
+
+        if(scrollY<60 && header.classList.contains("header-bg-white"))
+        {
+            header.classList.remove("header-bg-white")
+        }
+    }
+}
+
+let burgerButton = document.querySelector(".header__burger");
+let list = document.querySelector(".header__list");
+burgerButton.addEventListener("click",openCloseMenu);
+
+function openCloseMenu(event)
+{
+    event.preventDefault();
+    list.classList.toggle("header__list-openClose");
+}
+
